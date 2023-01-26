54b0d2af8cd48e4bb41b7da4ded6732d8f48d1ab
Your name01~ <poovareskkc@gmail.com>
2023-01-26 10:46:07 +0700

    kc1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,157 @@
+// const menufood = [
 
-export const Home = () => {
+//   [
+//     {
+//       "id": 1,
+//       "name": "ลาบหมู-เนื้อ-ไก่",
+//       "image": "https://img.wongnai.com/p/1920x0/2018/12/17/bf69cc77dfb94a5ab6df20ffb0622cd2.jpg",
+//       "price": 150,
+//       "category": "แนะนำ"
+//     },
+//     {
+//       "id": 2,
+//       "name": "ส้มตำไทยใส่ปู-ปลาร้า",
+//       "image": "https://img.wongnai.com/p/1920x0/2018/12/17/bf69cc77dfb94a5ab6df20ffb0622cd2.jpg",
+//       "price": 150,
+//       "category": "แกง"
+//     },
+//     {
+//       "id": 3,
+//       "name": "หมูย่าง น้ำจิ้มแจ่ว",
+//       "image": "https://tourdefood.net/wp-content/uploads/2020/11/%E0%B8%84%E0%B8%B0%E0%B8%99%E0%B9%89%E0%B8%B2-%E0%B9%80%E0%B8%AB%E0%B9%87%E0%B8%94%E0%B8%AB%E0%B8%AD%E0%B8%A1-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%A1%E0%B8%B1%E0%B8%99%E0%B8%AB%E0%B8%AD%E0%B8%A2-%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%94%E0%B8%B5%E0%B8%95%E0%B9%88%E0%B8%AD%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E.jpg",
+//       "price": 130,
+//       "category": "แนะนำ"
+//     },
+//     {
+//       "id": 4,
+//       "name": "ซุปหน่อไม้",
+//       "image": "https://tourdefood.net/wp-content/uploads/2020/11/%E0%B8%84%E0%B8%B0%E0%B8%99%E0%B9%89%E0%B8%B2-%E0%B9%80%E0%B8%AB%E0%B9%87%E0%B8%94%E0%B8%AB%E0%B8%AD%E0%B8%A1-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%A1%E0%B8%B1%E0%B8%99%E0%B8%AB%E0%B8%AD%E0%B8%A2-%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%94%E0%B8%B5%E0%B8%95%E0%B9%88%E0%B8%AD%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E.jpg",
+//       "price": 130,
+//       "category": "ผัด"
+//     },
+//     {
+//       "id": 5,
+//       "name": "ยำปลาร้ากุ้งสด",
+//       "image": "https://i.ytimg.com/vi/nh4nzZza4yE/maxresdefault.jpg",
+//       "price": 200,
+//       "category": "แนะนำ"
+//     },
+//     {
+//       "id": 6,
+//       "name": "ต้มแซ่บกระดูกอ่อน",
+//       "image": "https://i.ytimg.com/vi/nh4nzZza4yE/maxresdefault.jpg",
+//       "price": 200,
+//       "category": "ทอด"
+//     },
+//     {
+//       "id": 7,
+//       "name": "เสือร้องไห้",
+//       "image": "https://www.maeban.co.th/upfiles/blog/2644_37_22.jpg",
+//       "price": 130,
+//       "category": "ผัด"
+//     },
+//     {
+//       "id": 8,
+//       "name": "ต้มเล้งแซ่บ",
+//       "image": "https://i.ytimg.com/vi/Y-iH7nfeUCE/maxresdefault.jpg",
+//       "price": 130,
+//       "category": "ผัด"
+//     },
+//     {
+//       "id": 9,
+//       "name": "ก้อยเนื้อ",
+//       "image": "https://i.ytimg.com/vi/6yM0oog1iDQ/maxresdefault.jpg",
+//       "price": 130,
+//       "category": "ผัด"
+//     },
+//     {
+//       "id": 10,
+//       "name": "ส้มตำทอด",
+//       "image": "https://img-global.cpcdn.com/recipes/1ec6618e288a3f43/1200x630cq70/photo.jpg",
+//       "price": 250,
+//       "category": "ทอด"
+//     },
+//     {
+//       "id": 11,
+//       "name": "ส้มตำกุ้งสด",
+//       "image": "https://www.hongthongrice.com/v2/wp-content/uploads/2018/01/HTR-Deep-Fried-Shrimp-Cover.jpg",
+//       "price": 180,
+//       "category": "ทอด"
+//     },
+//     {
+//       "id": 12,
+//       "name": "ตำหอยดอง",
+//       "image": "https://food.mthai.com/app/uploads/2018/05/Lemongrass-Fried-Chicken.jpg",
+//       "price": 160,
+//       "category": "ทอด"
+//     },
+//     {
+//       "id": 13,
+//       "name": "ตำข้าวโพด",
+//       "image": "http://f.ptcdn.info/754/045/000/od2lq597lpF6HbXH4y4-o.jpg",
+//       "price": 180,
+//       "category": "แกง"
+//     },
+//     {
+//       "id": 14,
+//       "name": "ตำหอยแครง",
+//       "image": "https://www.ajinomotofoodservicethailand.com/wp-content/uploads/2019/01/21-2.jpg",
+//       "price": 180,
+//       "category": "แกง"
+//     },
+//     {
+//       "id": 15,
+//       "name": "ตำซั่ว",
+//       "image": "https://img-global.cpcdn.com/recipes/30d629683ebad9f5/1200x630cq70/photo.jpg",
+//       "price": 180,
+//       "category": "แกง"
+//     },
+//     {
+//       "id": 16,
+//       "name": "อ่อมปลาดุก",
+//       "image": "https://shopee.co.th/blog/wp-content/uploads/2021/03/%E0%B9%80%E0%B8%A1%E0%B8%99%E0%B8%B9%E0%B8%95%E0%B9%89%E0%B8%A1%E0%B8%88%E0%B8%B7%E0%B8%94%E0%B8%AB%E0%B8%A1%E0%B8%B9%E0%B8%AA%E0%B8%B1%E0%B8%9A.jpg",
+//       "price": 180,
+//       "category": "ต้ม"
+//     },
+//     {
+//       "id": 17,
+//       "name": "ลาบปลาดุก",
+//       "image": "https://thainipponfoods.com/wp-content/uploads/2022/01/Chicken-coconut-milk-pic-1.jpg",
+//       "price": 180,
+//       "category": "ต้ม"
+//     },
+//     {
+//       "id": 18,
+//       "name": "ต้มแซ่บหมูกระดูกอ่อน",
+//       "image": "https://www.khaosod.co.th/wpapp/uploads/2020/09/col10p1-2.jpg",
+//       "price": 180,
+//       "category": "ต้ม"
+//     }
+//   ]
+// ];
 
-  return (
-    <div className="px-4">
-     Home
-    </div>
-  )
-}
+
+
+
+
+
+export const Home = () => (
+  <div className="px-4">
+    <h1 className="text-3xl text-center mt-4">คุณบินตำแซ่บ</h1>
+    <p className="mt-2">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate saepe
+      commodi voluptates, magnam cum blanditiis ipsum! Quas perspiciatis ut
+      ullam qui impedit dolores tenetur error exercitationem! Officia ratione
+      esse at, odit cumque tempore voluptatem soluta consequuntur quasi earum
+      voluptatibus eveniet modi illum enim assumenda totam vitae dolores! Eum,
+      non. Eaque!
+    </p>
+    <img src="https://www.matichonacademy.com/wp-content/uploads/2021/03/thai-food-som-tum-papaya-salad.jpg" className="w-full mt-4 rounded-lg" />
+    
+    <h4 className="mt-4 mb-2 font-bold">รายการแนะนำ</h4>
+    
+  </div>
+);
+
+
+
