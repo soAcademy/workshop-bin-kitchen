73b24f9cf9d68df99935ad01b9e450bc2fc83496
Your name <teak.yuktanand@gmail.com>
2023-01-26 12:56:45 +0700

    feat add: useEffect

--- src/components/FoodMenuList.js
+++ src/components/FoodMenuList.js
@@ -2,7 +2,7 @@ const FoodMenuList = (props) => {
   return (
     <div>
       <h1 className="text-bold text-sky-500 p-2 text-3xl">{props.category}</h1>
-      {props.data.map((r) => {
+      {props?.data?.map((r) => {
         return (
           <div className="flex">
             <img src={r.image} className="h-[150px] w-[200px] rounded-lg p-2"></img>
