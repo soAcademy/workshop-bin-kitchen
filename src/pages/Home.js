2a7717eab747114d6969f14ab77589fa43d53065
Bond Rungrot <st122150@ait.asia>
2023-01-26 09:40:29 +0700

    init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
 
 export const Home = () => {
+  const info = {
+    description: "fdskafdsgfdsgjkfdlsgjfdslkgjfkdlsjgfkldsjgjklfdsjgklfdjg",
+    food: "gfkdsflgfdkl;sgfdkslgjfdkljgfkdlsgjfdslgkfdjglkfs",
+  }
+
+
+
 
   return (
     <div className="px-4">
+      
      Home
+
+
+
+
+
+
+
+
+
+
+
+    
     </div>
   )
 }
