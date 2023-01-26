90958b7117813e6f667793fb14a420c714ce781c
Phisit Siprasatthong <phisite@gmail.com>
2023-01-26 09:58:14 +0700

    feat: add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import banner from "../assets/banner.jpg";
+
+const Hero = () => {
+  const heroContent = {
+    title: "ร้านอาหารครัวคุณเก่ง",
+    description: `ร้านอาหารครัวคุณเก่งปรุงด้วยใจ เหมือนทำให้คนในครอบครัวทาน
+เราเปิดให้บริการตั้งแต่ปี พ.ศ. 2566 กว่า 0 ปีที่เรานำเสนอความ
+อร่อยแบบไทยแท้ เราคัดสรรวัตถุดิบชั้นดี ปลอดภัย ออร์แกนิค จาก
+เกษตรกรในพื้นที่สุโขทัย ปรุงด้วยสูตรลับต้นตำรับชาววังที่สืบทอด
+กันมารุ่นสู่รุ่น จัดแต่งมาบนจาน พร้อมเสิร์ฟความอร่อยให้กับคุณ`,
+  };
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-4">{heroContent.title}</h1>
+      <p className="mb-4">{heroContent.description}</p>
+      <img src={banner} alt="Food" className="m-auto" />
+    </>
+  );
+};
+
+export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
+import Hero from "../components/Hero";
 
 export const Home = () => {
-
   return (
     <div className="px-4">
-     Home
+      <Hero />
     </div>
-  )
-}
+  );
+};
