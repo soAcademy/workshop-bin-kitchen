f0c9306007e966fa002229730633cf4fd2afd334
Phisit Siprasatthong <phisite@gmail.com>
2023-01-29 21:22:58 +0700

    fix: reformat menu list for larger screen sizes

--- src/components/FoodMenuList.js
+++ src/components/FoodMenuList.js
@@ -16,25 +16,30 @@ const FoodMenuList = (props) => {
   //   });
   // }, []);
 
-  return props.menu
-    .filter((food) => food.category === props.category)
-    .map((food) => (
-      <div key={food.id} className="mb-4 flex gap-4">
-        <img
-          className="h-[72px] w-[72px] rounded-[10px] object-cover"
-          src={food.image}
-          alt={food.name}
-        />
-        <div className="flex flex-auto flex-col">
-          <div className="flex-auto">{food.name}</div>
-          <div className="text-red-600">฿{food.price}</div>
-        </div>
-        <button className="self-center rounded-[10px] bg-red-200 px-6 py-3 hover:bg-red-300">
-          เพิ่ม
-        </button>
-        {/* <div>{food.description}</div> */}
-      </div>
-    ));
+  return (
+    <div className="mb-4 grid gap-4 md:grid-cols-2">
+      {props.menu
+        .filter((food) => food.category === props.category)
+        .map((food) => (
+          <div key={food.id} className="flex gap-4">
+            <img
+              className="h-[72px] w-[72px] rounded-[10px] object-cover"
+              src={food.image}
+              alt={food.name}
+            />
+            <div className="flex flex-auto flex-col">
+              <div className="flex-auto">{food.name}</div>
+              <div className="text-red-600">฿{food.price}</div>
+            </div>
+            <button className="self-center rounded-[10px] bg-red-200 px-6 py-3 hover:bg-red-300">
+              เพิ่ม
+            </button>
+            {/* <div>{food.description}</div> */}
+          </div>
+        ))}
+    </div>
+  );
+  // <div className="grid gap-4 md:grid-cols-2"></div>
 };
 
 export default FoodMenuList;
