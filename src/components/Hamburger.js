5521b4a74549e33e6ebb8054fd3c097edd700efe
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-29 08:31:14 +0700

    update: underline the burger

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -5,13 +5,17 @@ const Hamburger = () => {
     <>
       <div className="bg-rose-100 px-8 py-4 flex flex-col gap-3 md:py-6 md:gap-6 md:py-9">
         <div className="text-lg md:text-3xl">
-          <button className="hover:text-rose-500">เมนูอาหาร</button>
+          <button className="hover:text-rose-500 hover:underline">
+            เมนูอาหาร
+          </button>
         </div>
         <div className="text-lg md:text-3xl">
-          <button className="hover:text-rose-500">รายการสั่งอาหาร</button>
+          <button className="hover:text-rose-500 hover:underline">
+            รายการสั่งอาหาร
+          </button>
         </div>
         <div className="text-lg md:text-3xl">
-          <button className="hover:text-rose-500">สถิติ</button>
+          <button className="hover:text-rose-500 hover:underline">สถิติ</button>
         </div>
       </div>
     </>
