dd35021f5bb2f5a5c68d2b04e1fd414441e3612c
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-30 09:41:42 +0700

    feat: add cart popup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,38 @@
-
 export const Home = () => {
+  const [foodMenus, setFoodMenus] = useState();
+  // const [categories, setCategories] = useState();
+
+  const [cart, setCart] = useState([]);
+  // [{productId: 1, quantity: 1, name: 'ผัดคะน้า'}, {productId: 2, quantity: 3, name: 'แกงส้ม'}]
+
+  const [toggleCartPopup, setToggleCartPopup] = useState(false);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://pastebin.com/raw/x1EY0NL9",
+    }).then((res) => {
+      console.log(res.data);
+      setFoodMenus(res.data);
+      // setCategories([...new Set(res.data?.map((r) => r.category))]);
+    });
+  });
 
   return (
     <div className="px-4">
-     Home
+      Home
+      {/* <FoodMenuList data={foodMenus} category="เมนูแนะนำ" /> */}
+      <FoodMenuGroup
+        foodMenus={foodMenus}
+        categories={[...new Set(res.data?.map((r) => r.category))]}
+        className="px-4 mt-4"
+      />
+      <CartPopup
+        cart={cart}
+        setCart={setCart}
+        toggleCartPopup={toggleCartPopup}
+        setToggleCartPopup={setToggleCartPopup}
+      />
     </div>
-  )
-}
+  );
+};
