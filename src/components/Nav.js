4a093d36f0644b23688bda4cd2ca91894ce79fa4
Firstweera <firstweera357@gmail.com>
2023-02-07 01:18:22 +0700

    feat: chart2&3

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,8 +6,8 @@ export const Nav = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <div className="shadow-lg w-full h-16 fixed top-0 left-0">
-      <div className="md:flex justify-between bg-white py-4 px-7">
+    <div className="shadow-lg w-full h-16 fixed top-0 left-0 z-50">
+      <div className="md:flex justify-between bg-white py-5 px-7">
         <button
           onClick={() => setToggle(!toggle)}
           className="text-3xl absolute cursor-pointer md:hidden"
@@ -20,7 +20,7 @@ export const Nav = () => {
           </Link>
         </div>
         <ul
-          className={`md:flex md:items-center md:justify-end md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in ${
+          className={`md:flex md:items-center md:justify-end md:pb-0 pb-12 absolute md:static bg-white z-0 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in ${
             toggle ? "top-15 " : "top-[-490px]"
           }`}
         >
--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -4,54 +4,109 @@ import axios from "axios";
 import ReactECharts from "echarts-for-react";
 
 const Stats = () => {
-  const [orders, setOrders] = useState([]);
-  const [chart1, setChart1] = useState({});
-  const [chart2, setChart2] = useState({});
+  const [chart1, setChart1] = useState([]);
+  const [chart2, setChart2] = useState([]);
+  const [chart3, setChart3] = useState([]);
+  // console.log("Chart1: ", chart1);
+  // console.log("Chart2: ", chart2);
+  console.log("Chart3: ", chart3);
+
+  // useEffect(() => {
+  //   const config = {
+  //     method: "post",
+  //     // maxBodyLength: Infinity,
+  //     url: "https://sprinttech-food-menu-api-iinykauowa-uc.a.run.app/get-orders",
+  //     headers: {},
+  //   };
+
+  //   axios(config)
+  //     .then(function (response) {
+  //       // console.log("testData: ", JSON.stringify(response.data));
+  //       const getOrderedItems = () => {
+  //         const totalOrder = Object.values(
+  //           response.data
+  //             .flatMap((r) => r.items)
+  //             .reduce((acc, i) => {
+  //               if (!acc[i.name]) {
+  //                 acc[i.name] = {
+  //                   name: i.name,
+  //                   qty: 0,
+  //                   totalPrice: i.totalPrice,
+  //                 };
+  //               }
+  //               acc[i.name].qty += i.quantity;
+  //               acc[i.name].totalPrice += acc[i.name].totalPrice;
+  //               return acc;
+  //             }, {})
+  //         ).sort((a, b) => b.qty - a.qty);
+  //         return totalOrder;
+  //       };
+  //       setChart1(getOrderedItems);
+  //       console.log("getOrderedItems: ", getOrderedItems());
+
+  //       const totalPrice = getOrderedItems().map((r) => ({
+  //         name: r.name,
+  //         value: r.total_price,
+  //       }));
+  //       setChart2(totalPrice);
+  //       console.log("totalPrice", totalPrice);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }, []);
 
   useEffect(() => {
-    const getData = async () => {
+    const fetchData = async () => {
       try {
-        const config = {
-          method: "post",
-          url: "https://sprinttech-food-menu-api-iinykauowa-uc.a.run.app/get-orders",
-          headers: {},
-        };
+        const response = await axios.post(
+          "https://sprinttech-food-menu-api-iinykauowa-uc.a.run.app/get-orders"
+        );
+        const getOrderedItems = response.data
+          .reduce((acc, r) => {
+            r.items.forEach((r) => {
+              const filterId = acc.findIndex(
+                (order) => order.menu_id === r.menu_id
+              );
+              if (filterId >= 0) {
+                acc[filterId].quantity += r.quantity;
+                acc[filterId].total_price += r.total_price;
+              } else {
+                acc.push(r);
+              }
+            });
+            return acc;
+          }, [])
+          .sort((a, b) => b.quantity - a.quantity);
+        setChart1(getOrderedItems);
+        console.log("getOrderedItems: ", getOrderedItems);
+        setChart2(
+          getOrderedItems.map((r) => ({ name: r.name, value: r.total_price }))
+        );
 
-        const response = await axios(config);
-        setOrders(response.data);
-        setChart1(response.data);
-        setChart2(response.data);
+        const tableId = [...new Set(response.data.map((r) => r.table_id))]
+          .sort((a, b) => a - b)
+          .map((i) => {
+            const filterValueTable = response.data.filter(
+              (r) => r.table_id === i
+            ).length;
+            return { name: i, value: filterValueTable };
+          });
+
+        setChart3(tableId);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
-
-    getData();
+    fetchData();
   }, []);
-
-  const getOrderedItems = () => {
-    const totalOrder = Object.values(
-      orders
-        .flatMap((r) => r.items)
-        .reduce((acc, i) => {
-          if (!acc[i.name]) {
-            acc[i.name] = { name: i.name, value: 0 };
-          }
-          acc[i.name].value += i.quantity;
-          return acc;
-        }, {})
-    ).sort((a, b) => b.value - a.value);
-
-    return totalOrder;
-  };
-  console.log("getOrderedItems: ", getOrderedItems());
 
   const option1 = {
     xAxis: {
       type: "category",
       name: "menus",
       axisLabel: { interval: 0, rotate: 30 },
-      data: getOrderedItems().map((r) => r.name),
+      data: chart1.map((r) => r.name),
     },
     yAxis: {
       type: "value",
@@ -60,7 +115,7 @@ const Stats = () => {
     series: [
       {
         type: "bar",
-        data: getOrderedItems().map((r) => r.value),
+        data: chart1.map((r) => r.quantity),
       },
     ],
     tooltip: {
@@ -68,17 +123,95 @@ const Stats = () => {
     },
   };
 
+  const option2 = {
+    title: {
+      text: "TotalPriceByMenus",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      orient: "vertical",
+      left: "left",
+      top: "10%",
+      itemGap: 5,
+      itemHeight: 8,
+    },
+    series: [
+      {
+        name: "Menus",
+        type: "pie",
+        radius: "50%",
+        data: chart2,
+        left: "30%",
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+        label: {
+          show: false,
+        },
+      },
+    ],
+  };
+
+  const option3 = {
+    title: {
+      text: "TotalOrdersByTable",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      orient: "vertical",
+      left: "left",
+      top: "10%",
+      itemGap: 5,
+      itemHeight: 8,
+    },
+    series: [
+      {
+        name: "Table",
+        type: "pie",
+        radius: "50%",
+        data: chart3,
+        left: "30%",
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+        label: {
+          show: false,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="px-4 w-full">
-      <div className="h-14 mt-20">
+    <div className="px-4 w-full mt-20">
+      <div>
         <Link to="/stats">
           <p className="font-normal font-[Prompt] text-3xl text-center">
             สถิติ
           </p>
         </Link>
       </div>
-      <div>
+      <div className="w-full pb-5">
         <ReactECharts option={option1} />
+      </div>
+      <div className="w-full mt-10">
+        <ReactECharts option={option2} />
+      </div>
+      <div className="w-full mt-10">
+        <ReactECharts option={option3} />
       </div>
     </div>
   );
